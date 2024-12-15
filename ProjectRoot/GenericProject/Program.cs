e6fdb7a74d2f1450bfa1d229f234cdc8dab919c6
Garik Mikayelyan <garik.2002.mgt@gmail.com>
2024-12-16 02:01:12 +0400

    to amend

--- ProjectRoot/GenericProject/Program.cs
+++ ProjectRoot/GenericProject/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
+using System.Linq;
 using System.Text.Json.Serialization;
+using System.Net.Http.Json;
 
 var person = new Person
 {
@@ -11,18 +14,21 @@ var person = new Person
     Child = new() { Name = "Child", Age = 12, Email = "Child@Cm.com" }
 };
 
-// Serialize the person object to a JSON string
-string jsonString = JsonSerializer.Serialize(person, IgnoreJsonIgnoreConverter<Person>.CreateSerializerOptions());
-Console.WriteLine("Serialized JSON:");
-Console.WriteLine(jsonString);
+var copy = (Person)person.Clone();
 
-// Deserialize the JSON string back into a Person object
-var deserializedPerson = JsonSerializer.Deserialize<Person>(jsonString,
-    IgnoreJsonIgnoreConverter<Person>.CreateSerializerOptions())!;
-Console.WriteLine("\nDeserialized Object:");
-Console.WriteLine(deserializedPerson);
+Print(person, copy);
+Console.WriteLine(new string('_', 50));
+copy.Email = default!;
+copy.Child.Name = "adsadsd";
+Print(person, copy);
 
-public class Person
+static void Print(Person person, Person copy)
+{
+    Console.WriteLine(person);
+    Console.WriteLine(copy);
+}
+
+public class Person : ICloneable
 {
     public string Name { get; set; } = "";
     [JsonIgnore]
@@ -30,11 +36,23 @@ public class Person
     public string Email { get; set; } = "";
     [JsonIgnore]
     public Person? Child { get; set; }
+    [JsonIgnore]
+    public List<Person> people = [];
 
     public override string ToString()
     {
         return $"{{Name={Name}, Age={Age}, Email={Email}" +
             $"\nChild={Child?.ToString() ?? "null"}}}";
+    }
+
+    public object Clone()
+    {
+        var clone = this.DeepCopy();
+        clone.Child = this.Child.DeepCopy();
+        clone.people = this.people.DeepCopy();
+        clone.Age = this.Age.DeepCopy();
+
+        return clone;
     }
 }
 
@@ -42,63 +60,10 @@ public static class DeepCopyExtension
 {
     public static T DeepCopy<T>(this T source)
     {
-        if (source == null)
-            throw new ArgumentNullException(nameof(source));
+        var json = JsonSerializer.Serialize(source);
+        var copy = JsonSerializer.Deserialize<T>(json)!;
 
-        return (T)DeepCopyObject(source);
-    }
-
-    private static object DeepCopyObject(object source)
-    {
-        var sourceType = source.GetType();
-
-        if (sourceType.IsPrimitive || sourceType == typeof(string))
-            return source;
-
-        // Handle collections (IEnumerable, excluding strings)
-        if (source is IEnumerable enumerable && !(source is string))
-        {
-            var elementType = source.GetType().GetElementType() ?? sourceType.GetGenericArguments().First();
-            var copyList = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(elementType));
-
-            foreach (var item in enumerable)
-            {
-                copyList.Add(DeepCopyObject(item));
-            }
-
-            return copyList;
-        }
-
-        // Handle creating a new instance of the source type
-        var target = Activator.CreateInstance(sourceType);
-
-        // Get all properties and fields of the source type
-        var members = sourceType.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .Where(m => m.MemberType == MemberTypes.Field || m.MemberType == MemberTypes.Property)
-            .ToList();
-
-        foreach (var member in members)
-        {
-            if (member is PropertyInfo propertyInfo)
-            {
-                // If the property has a getter, copy its value
-                if (propertyInfo.CanRead)
-                {
-                    var value = propertyInfo.GetValue(source);
-                    propertyInfo.SetValue(target, DeepCopyObject(value));
-                }
-            }
-            else if (member is FieldInfo fieldInfo)
-            {
-                // For fields, directly get and set the value
-                var value = fieldInfo.GetValue(source);
-                fieldInfo.SetValue(target, DeepCopyObject(value));
-            }
-        }
-
-        return target;
+        return copy;
     }
 }
-
-
 
