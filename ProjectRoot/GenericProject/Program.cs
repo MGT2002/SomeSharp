71d49130144476545a692314f5cc6d904a982f8c
Garik Mikayelyan <garik.2002.mgt@gmail.com>
2024-12-17 07:40:26 +0400

    GitHub DeepCopy.

--- ProjectRoot/GenericProject/Program.cs
+++ ProjectRoot/GenericProject/Program.cs
@@ -1,17 +1,31 @@
-﻿using GenericProject;
+﻿using DeepCopy;
 using System.Text.Json.Serialization;
 
-var person = new Person
+var person = new Person(1, 2)
 {
     Name = "John Doe",
     Age = 30,
     Email = "johndoe@example.com",
-    Child = new() { Name = "Child", Age = 12, Email = "Child@Cm.com" }
+    Child = new(1, 2) { Name = "Child", Age = 12, Email = "Child@Cm.com" }
+};
+var person2 = new Person(1, 2)
+{
+    Name = "2",
+    Age = 30,
+    Email = "2",
+    Child = new(1, 2) { Name = "Child2", Age = 12, Email = "Child2@Cm.com" }
+};
+var person3 = new Person(1, 2)
+{
+    Name = "3",
+    Age = 30,
+    Email = "3",
+    Child = new(3, 4) { Name = "Child3", Age = 12, Email = "Child3@Cm.com" }
 };
 
-person.people.AddRange([200, 100]);
+person.people.AddRange([person2, person3]);
 
-var copy = (Person)person.DeepCopy();
+var copy = DeepCopier.Copy(person);
 
 Print(person, copy);
 Console.WriteLine(new string('_', 50));
@@ -38,10 +52,18 @@ public class Person
     [JsonIgnore]
     public Person? Child { get; set; }
     [JsonIgnore]
-    public List<int> people = [];
+    public List<Person> people = [];
+    [JsonIgnore]
+    private IEnumerable<object> some = [1, "asdsad", null, new List<int>[1, 3, 6, 1]];
+
+    public Person(int a, int b)
+    {
+        
+    }
 
     public override string ToString()
     {
+        string some = string.Join(',', this.some);
         string peopleText = "people: [\n";
         foreach (var i in people)
         {
@@ -51,6 +73,6 @@ public class Person
         peopleText += "]";
 
         return $"{{\nName={Name}, Age={Age}, Email={Email}" +
-            $"\nChild={Child?.ToString() ?? "null"}\n{peopleText}\n}}";
+            $"\nChild={Child?.ToString() ?? "null"}\n{peopleText}\nsome: {some}}}";
     }
 }
