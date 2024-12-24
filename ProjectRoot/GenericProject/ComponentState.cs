0110d6ebdb498e64ff1782733d11d39d9d421162
Garik Mikayelyan <garik.2002.mgt@gmail.com>
2024-12-24 22:31:31 +0400

    Smart enums setup.

---
+++ ProjectRoot/GenericProject/ComponentState.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.SmartEnum;
+
+namespace GenericProject;
+
+public class ComponentState : SmartEnum<ComponentState, int>
+{
+    public static readonly ComponentState New = new(nameof(New).ToLower(), 1);
+    public static readonly ComponentState InProgress = new(nameof(InProgress).ToLower(), 2);
+    public static readonly ComponentState Done = new(nameof(Done).ToLower(), 3);
+
+    private ComponentState(string name, int value) : base(name, value) { }
+}
--- ProjectRoot/GenericProject/Program.cs
+++ ProjectRoot/GenericProject/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Project Root!");
+﻿using GenericProject;
+
+ComponentState current = ComponentState.New;
+
+switch (current.Name)
+{
+    case nameof(ComponentState.New):
+        break;
+    case nameof(ComponentState.Done):
+        break;
+    default:
+		break;
+}
+
+public static class Constatns
+{
+    public static readonly PageInfo AdminPage = new(nameof(AdminPage), "/admin", "Admin page");
+    public static readonly PageInfo EmployeePage = new("E", "/admin", "Admin page");
+
+    public record PageInfo(string Name, string Url, string AdditionalInfo);
+}
