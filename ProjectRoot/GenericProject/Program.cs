996669de30fd4aa82d77cc874815a603f490d4a2
Garik Mikayelyan <garik.2002.mgt@gmail.com>
2024-12-26 02:18:52 +0400

    ClimbStairs

--- ProjectRoot/GenericProject/Program.cs
+++ ProjectRoot/GenericProject/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Project Root!");
+﻿Enumerable.Range(1, 45).ToList().ForEach(i => Console.WriteLine(ClimbStairs(i)));
+
+// to memoize the recursive function
+int ClimbStairs(int n)
+{
+	int a = 0, b = 1, c = 0;
+    for (int i = 0; i < n; i++)
+	{
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
