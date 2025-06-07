c0b4075f37cdb1229b0318131974e284f4c11e83
Garik Mikayelyan <Garik.Mikayelyan@quitt.ch>
2025-06-07 19:42:44 +0400

    Initial.

---
+++ ProjectRoot/GenericProject/LogExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericProject;
+
+public static class LogExtensions
+{
+    public static T Log<T>(this T obj, string start = "", string end = "")
+    {
+        Console.WriteLine(start + obj + end);
+
+        return obj;
+    }
+
+    public static T SeriLog<T>(this T obj, string start = "", string end = "")
+    {
+        try
+        {
+            Serilog.Log.Information(start + obj + end);
+        }
+        finally
+        {
+            Serilog.Log.CloseAndFlush(); // Ensure all logs are written
+        }
+
+        return obj;
+    }
+}
--- ProjectRoot/GenericProject/Program.cs
+++ ProjectRoot/GenericProject/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Project Root!");
+﻿using GenericProject;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Debug() // Set minimum log level
+           .WriteTo.Console() // Log to console
+           .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Log to daily files
+           .CreateLogger();
+
+var scpecialZipCode = "AAAA";
+
+var result = new ApiResult() { Users = [new() { Address = new() { ZipCode = "BBBB" } }, new() { Address = new()}] };
+
+if (result.Log("1->").Users.First().Address.Log("2->").ZipCode.Log("Zip->")
+    == scpecialZipCode.SeriLog("Special is: "))
+{
+    "IsSpecial".Log();
+}
+else
+{
+    0.Log();
+}
+
+class ApiResult
+{
+    public required User[] Users { get; set; }
+}
+
+class User
+{
+    public required Address Address { get; set; }
+}
+
+class Address
+{
+    public string ZipCode { get; set; } = "";
+}
