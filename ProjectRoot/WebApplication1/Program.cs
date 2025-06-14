bfc7661dd0db9cf0cd52d285e9e5fcf948c8d5bd
Garik Mikayelyan <Garik.Mikayelyan@quitt.ch>
2025-06-14 14:39:21 +0400

    Added http.

--- ProjectRoot/WebApplication1/Program.cs
+++ ProjectRoot/WebApplication1/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+    return "Hello World!";
+});
+
+app.MapGet("/square/{num}", (int num) => {
+    return num * num;
+});
 
 app.Run();
